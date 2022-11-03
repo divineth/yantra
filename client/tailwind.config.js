780df99a8192480ea05a716e7347f9b6386e7c1d
divineth <divinethomson@outlook.com>
2022-11-03 18:12:02 +0000

    Use src directory

--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -3,9 +3,9 @@ const plugin = require("tailwindcss/plugin");
 
 module.exports = {
   content: [
-    "./app/**/*.{js,ts,jsx,tsx}",
-    "./pages/**/*.{js,ts,jsx,tsx}",
-    "./components/**/*.{js,ts,jsx,tsx}",
+    "./src/app/**/*.{js,ts,jsx,tsx}",
+    "./src/pages/**/*.{js,ts,jsx,tsx}",
+    "./src/components/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
     extend: {
