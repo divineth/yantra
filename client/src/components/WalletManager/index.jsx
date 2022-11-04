76378407120f10bcc36c092cb6c95f23696aa190
divineth <divinethomson@outlook.com>
2022-11-04 02:07:26 +0000

    Complete wallet connect modal

--- client/src/components/WalletManager/index.jsx
+++ client/src/components/WalletManager/index.jsx
@@ -4,32 +4,43 @@ import style from "./wallet-manager.module.css";
 import MetaMask from "../../assets/images/metamask.png";
 import WalletConnect from "../../assets/images/walletConnect.svg";
 import Coinbase from "../../assets/images/coinbase.svg";
+import CopySymbol from "../../assets/images/copy.svg";
+import ExternalLink from "../../assets/images/external-link.svg";
+import { CopyToClipboard } from "react-copy-to-clipboard";
+
 
 const providers = [
-    {
-        options: {},
-        displayName: "MetaMask",
-        icon: MetaMask.src,
-    },
-    {
-        options: { provider: "walletconnect" },
-        displayName: "Coinbase Wallet",
-        icon: Coinbase.src,
-    },
-    {
-        options: { provider: "walletconnect" },
-        displayName: "WalletConnect",
-        icon: WalletConnect.src,
-    },
-
-]
+  {
+    options: {},
+    displayName: "MetaMask",
+    icon: MetaMask.src,
+  },
+  {
+    options: { provider: "walletconnect" },
+    displayName: "Coinbase Wallet",
+    icon: Coinbase.src,
+  },
+  {
+    options: { provider: "walletconnect" },
+    displayName: "WalletConnect",
+    icon: WalletConnect.src,
+  },
+];
 
 function WalletManager({ isOpen, onCloseModal }) {
+  const [isAuthenticated, setIsAuthenticated] = useState(false);
+  const walletAddress = "0xabcd";
 
-    const [isAuthenticated, setIsAuthenticated] = useState(false);
-    const walletAddress = "0xabcd";
+  const [copied, setCopied] = useState(false);
 
-    return(
+  const addressCopied = () => {
+    setCopied(true);
+    setTimeout(() => {
+      setCopied(false);
+    }, 2000);
+  };
+
+  return (
     <>
       <Transition appear show={isOpen && !isAuthenticated} as={Fragment}>
         <Dialog as="div" className={style.wallet_dialog} onClose={onCloseModal}>
@@ -56,15 +67,15 @@ function WalletManager({ isOpen, onCloseModal }) {
               <div className={style.wallet_dialog_content}>
                 <div className={style.wallet_dialog_header}>
                   <div className={style.wallet_dialog_header_content}>
-                      <Dialog.Title as="h3" className={style.wallet_dialog_title}>
-                        Select a wallet
-                      </Dialog.Title>
-                      <span
-                        onClick={onCloseModal}
-                        className={style.wallet_dialog_close}
-                      >
-                        &times;
-                      </span>
+                    <Dialog.Title as="h3" className={style.wallet_dialog_title}>
+                      Select a wallet
+                    </Dialog.Title>
+                    <span
+                      onClick={onCloseModal}
+                      className={style.wallet_dialog_close}
+                    >
+                      &times;
+                    </span>
                   </div>
                 </div>
 
@@ -72,7 +83,7 @@ function WalletManager({ isOpen, onCloseModal }) {
                   <ul className={style.wallet_dialog_providers_list}>
                     {providers.map(({ options, displayName, icon }, key) => (
                       <li key={key}>
-                        <button onClick={() => setIsAuthenticated(false)}>
+                        <button onClick={() => setIsAuthenticated(true)}>
                           <img src={icon} alt={displayName} />
                           <span>{displayName}</span>
                         </button>
@@ -110,22 +121,27 @@ function WalletManager({ isOpen, onCloseModal }) {
             >
               <div className={style.wallet_dialog_content}>
                 <div className={style.wallet_dialog_header}>
-                  <Dialog.Title as="h3" className={style.wallet_dialog_title}>
-                    Account
-                  </Dialog.Title>
+                  <div className={style.wallet_dialog_header_content}>
+                    <Dialog.Title as="h3" className={style.wallet_dialog_title}>
+                      Account
+                    </Dialog.Title>
 
-                  <span
-                    onClick={onCloseModal}
-                    className={style.wallet_dialog_close}
-                  >
-                    &times;
-                  </span>
+                    <span
+                      onClick={onCloseModal}
+                      className={style.wallet_dialog_close}
+                    >
+                      &times;
+                    </span>
+                  </div>
                 </div>
 
                 <div className={style.wallet_dialog_body}>
                   <div className={style.wallet_dialog_provider}>
-                    <p>Connected with X</p>
+                    <p>Connected to $YANTRA</p>
                     <button
+                      onClick={() => {
+                        setIsAuthenticated(false);
+                      }}
                       className={style.wallet_dialog_disconnect}
                     >
                       Disconnect
@@ -135,16 +151,18 @@ function WalletManager({ isOpen, onCloseModal }) {
                   <div className={style.wallet_dialog_address}>
                     <p>{walletAddress}</p>
                     <div className={style.wallet_dialog_address_copy_etherscan}>
+                      <CopyToClipboard
+                        text={walletAddress}
+                        onCopy={() => addressCopied()}
+                      >
                         <p className={style.wallet_dialog_bottom_text}>
-                          <img
-                            src={require("../../assets/images/copy.svg")}
-                            alt="etherscan"
-                          />
-                          <span>Copy address</span>
-                          {true ? (
+                          <img src={CopySymbol.src} alt="etherscan" />
+                          <span>Copy Address</span>
+                          {copied ? (
                             <span className={style.tooltip}>Copied.</span>
                           ) : null}
                         </p>
+                      </CopyToClipboard>
 
                       <p className={style.wallet_dialog_bottom_text}>
                         <a
@@ -152,10 +170,7 @@ function WalletManager({ isOpen, onCloseModal }) {
                           target="_blank"
                           rel="noopener noreferrer"
                         >
-                          <img
-                            src={require("../../assets/images/xternal-link.svg")}
-                            alt="etherscan"
-                          />
+                          <img src={ExternalLink.src} alt="etherscan" />
                           <span>View on Etherscan</span>
                         </a>
                       </p>
@@ -166,9 +181,9 @@ function WalletManager({ isOpen, onCloseModal }) {
             </Transition.Child>
           </div>
         </Dialog>
-      </Transition>            
+      </Transition>
     </>
-    )
+  );
 }
 
 export default WalletManager;
